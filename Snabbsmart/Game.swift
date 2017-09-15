02507924bbe636fbbbd53a7d0a4c3218e04ad68b
Viktor Johansson <viktorjohanssonn@outlook.com>
2017-09-15 23:23:23 +0200

    Refactor Game to use GameHandler

--- Snabbsmart/Game.swift
+++ Snabbsmart/Game.swift
@@ -1,18 +1,65 @@
 import Foundation
 
+struct Player {
+    var streak: Int = 0
+    var gamePosition: Int = 0
+    var questionPosition: Int = 0
+    var gameScore: Int = 0
+    var questionScore:Int = 0
+}
 
+struct Game {
+    var questionCount: Int = 0
+    var currentPlayer: Player = Player()
+    var currentQuestionNumber: Int = 1
+}
 
-class Game {
-    let questionCount: Int
-    let token: String
+class GameHandler {
+    static let shared = GameHandler()
 
-    init(questionCount: Int, token: String) {
-        self.questionCount = questionCount
-        self.token = token
+    func startQuestion(in game: Game, withData data: Data) -> Game {
+        return handleEvent(in: game, withData: data)
     }
 
-    func startQuestion(withData data: Data) -> Int {
+    func questionAnswerable(in game: Game, withData data: Data) -> Game {
+        return handleEvent(in: game, withData: data)
+    }
+
+    func endQuestion(in game: Game, withData data: Data) -> Game {
+        return handleEvent(in: game, withData: data)
+    }
+
+    fileprivate func handleEvent(in game: Game, withData data: Data) -> Game {
+        var game = game
+        parseDataToGame(data: data, game: &game)
+        return game
+    }
+
+    fileprivate func parseDataToGame(data: Data, game: inout Game) {
         let json = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-        return json["questionNumber"] as! Int
+
+        if let questionNumber = json["questionNumber"] as? Int {
+            game.currentQuestionNumber = questionNumber
+        }
+
+        if let streak = json["myStreak"] as? Int {
+            game.currentPlayer.streak = streak
+        }
+
+        if let gamePosition = json["myGamePosition"] as? Int {
+            game.currentPlayer.gamePosition = gamePosition
+        }
+
+        if let questionPosition = json["myQuestionPosition"] as? Int {
+            game.currentPlayer.questionPosition = questionPosition
+        }
+
+        if let gameScore = json["myGameScore"] as? Int {
+            game.currentPlayer.gameScore = gameScore
+        }
+
+        if let questionScore = json["myQuestionScore"] as? Int {
+            game.currentPlayer.questionScore = questionScore
+        }
     }
 }
--- Snabbsmart/GameTests.swift
+++ Snabbsmart/GameTests.swift
@@ -1,29 +1,57 @@
 import XCTest
 @testable import Snabbsmart
 class GameTests: XCTestCase {
-    
+    var gameHandler: GameHandler!
+    var game: Game!
+
     func testExample() {
-
     }
 
-
-    func testInitilizationWithQuestionCountAndToken() {
-        let game = Game(questionCount: 3, token: "123asd")
-
-        XCTAssertEqual(3, game.questionCount)
-        XCTAssertEqual("123asd", game.token)
+    override func setUp() {
+        super.setUp()
+        game = Game()
+        gameHandler = GameHandler.shared
     }
-
     func testStartQuestionWithDataWillReturnCurrentQuestionNumber() {
-        let game = Game(questionCount: 3, token: "123asd")
-
         let json: [String: Any] = [
             "questionNumber": 17
         ]
 
         let data = try! JSONSerialization.data(withJSONObject: json, options: [])
-        let questionNumber = game.startQuestion(withData: data)
+        game = gameHandler.startQuestion(in: game, withData: data)
 
-        XCTAssertEqual(17, questionNumber)
+        XCTAssertEqual(17, game.currentQuestionNumber)
+    }
+
+    func testQuestionAnswerableWithDataWillReturnCurrentQuestionNumber() {
+        let json: [String: Any] = [
+            "questionNumber": 17
+        ]
+
+        let data = try! JSONSerialization.data(withJSONObject: json, options: [])
+        game = gameHandler.questionAnswerable(in: game, withData: data)
+
+        XCTAssertEqual(17, game.currentQuestionNumber)
+    }
+
+    func testEndQuestionWithDataWillReturnQuestionResult() {
+        let json: [String: Any] = [
+            "questionNumber": 17,
+            "myStreak": 2,
+            "myGamePosition": 1,
+            "myQuestionPosition": 2,
+            "myGameScore": 2094,
+            "myQuestionScore": 710
+        ]
+
+        let data = try! JSONSerialization.data(withJSONObject: json, options: [])
+        game = gameHandler.endQuestion(in: game, withData: data)
+
+        XCTAssertEqual(17, game.currentQuestionNumber)
+        XCTAssertEqual(2, game.currentPlayer.streak)
+        XCTAssertEqual(1, game.currentPlayer.gamePosition)
+        XCTAssertEqual(2, game.currentPlayer.questionPosition)
+        XCTAssertEqual(2094, game.currentPlayer.gameScore)
+        XCTAssertEqual(710, game.currentPlayer.questionScore)
     }
 }
